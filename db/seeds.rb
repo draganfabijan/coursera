b397b63ea642c1c24e1ffa61b8f97fa86d80572c
Dragan Fabijan <dragan.fabijan@gmail.com>
2023-07-28 07:41:25 +1000

    Update seed file with categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,19 @@
 # Create Verticals
-['Health & Fitness', 'Business', 'Music'].each { |name| Vertical.create(name:) }
+verticals = ['Health & Fitness', 'Business', 'Music'].map do |name|
+  Vertical.find_or_create_by!(name: name)
+end
+
+# Define categories
+categories = [
+  {name: 'Booty & Abs', vertical: verticals[0], state: 'active'},
+  {name: 'Full Body', vertical: verticals[0], state: 'active'},
+  {name: 'Advertising', vertical: verticals[1], state: 'active'},
+  {name: 'Writing', vertical: verticals[1], state: 'active'},
+  {name: 'Singing', vertical: verticals[2], state: 'active'},
+  {name: 'Music Fundamentals', vertical: verticals[2], state: 'active'}
+]
+
+# Create categories
+categories.each do |category_attrs|
+  Category.find_or_create_by!(category_attrs)
+end
