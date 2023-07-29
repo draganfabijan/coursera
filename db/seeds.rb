5a647998a7064ec089b20ed6e3ee01330f69cc95
Dragan Fabijan <dragan.fabijan@gmail.com>
2023-07-29 11:16:54 +1000

    Update seed file and add blank lines at the end of file

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,70 +29,70 @@ puts 'Creating Courses'
 # Define courses
 courses = [
   {
-    name: "Loose the Gutt, keep the Butt",
-    author: "Anowa",
+    name: 'Loose the Gutt, keep the Butt',
+    author: 'Anowa',
     category: categories[0],
-    state: "active"
+    state: 'active'
   },
   {
-    name: "BrittneBabe Fitness Transformation",
-    author: "Brittnebabe",
+    name: 'BrittneBabe Fitness Transformation',
+    author: 'Brittnebabe',
     category: categories[0],
-    state: "active"
+    state: 'active'
   },
   {
-    name: "BTX: Body Transformation Extreme",
-    author: "Barstarzz",
+    name: 'BTX: Body Transformation Extreme',
+    author: 'Barstarzz',
     category: categories[1],
-    state: "active"
+    state: 'active'
   },
   {
-    name: "Facebook Funnel Marketing",
-    author: "Russell Brunson",
+    name: 'Facebook Funnel Marketing',
+    author: 'Russell Brunson',
     category: categories[2],
-    state: "active"
+    state: 'active'
   },
   {
-    name: "Build a Wild Audience",
-    author: "Tim Nilson",
+    name: 'Build a Wild Audience',
+    author: 'Tim Nilson',
     category: categories[2],
-    state: "active"
+    state: 'active'
   },
   {
-    name: "Editorial Writing Secrets",
-    author: "J. K. Rowling",
+    name: 'Editorial Writing Secrets',
+    author: 'J. K. Rowling',
     category: categories[3],
-    state: "active"
+    state: 'active'
   },
   {
-    name: "Scientific Writing",
-    author: "Stephen Hawking",
+    name: 'Scientific Writing',
+    author: 'Stephen Hawking',
     category: categories[3],
-    state: "active"
+    state: 'active'
   },
   {
-    name: "Vocal Training 101",
-    author: "Linkin Park",
+    name: 'Vocal Training 101',
+    author: 'Linkin Park',
     category: categories[4],
-    state: "active"
+    state: 'active'
   },
   {
-    name: "Music Production",
-    author: "Lady Gaga",
+    name: 'Music Production',
+    author: 'Lady Gaga',
     category: categories[4],
-    state: "active"
+    state: 'active'
   },
   {
-    name: "Learn the Piano",
-    author: "Lang Lang",
+    name: 'Learn the Piano',
+    author: 'Lang Lang',
     category: categories[5],
-    state: "active"
+    state: 'active'
   },
   {
-    name: "Become a guitar hero",
-    author: "Jimmy Page",
+    name: 'Become a guitar hero',
+    author: 'Jimmy Page',
     category: categories[5],
-    state: "active"
+    state: 'active'
   }
 ]
 
